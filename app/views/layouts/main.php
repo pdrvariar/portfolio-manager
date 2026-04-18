53b71b016d701f507c2a03210c0102fd1e55a54a
Pablo Damasceno Rattes <pdrvariar@gmail.com>
2026-04-18 15:12:55 -0300

    Enhance CSS loading; add cache-busting query string to stylesheet link for improved asset management

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -83,7 +83,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     
 
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css?v=<?php echo filemtime(dirname(__DIR__, 3) . '/public/css/style.css'); ?>">
 
     <?php echo $additional_css ?? ''; ?>
 </head>
